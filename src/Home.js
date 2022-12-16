712b4fdd02b54aac3b529b6338d80d496a9f94a0
Ankit <ankitanand1974@gmail.com>
2022-12-16 21:44:10 +0530

    started from beginning

---
+++ src/Home.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img className="home__image" src={require("./bg.jpg")} alt="" />
+
+        <div className="home__row">
+          <Product
+            title={"Marvel's Spider-Man: Miles Morales (PS4)"}
+            price={1988}
+            image="https://m.media-amazon.com/images/I/811umGHYORL._AC_UY327_FMwebp_QL65_.jpg"
+            rating={5}
+          />
+        </div>
+
+        <div className="home__row">
+          <Product 
+            title={"MSI GeForce RTX 3050 Ventus 8GB GDDR6"}
+            price={28499}
+            image="https://m.media-amazon.com/images/I/61EygjtA8NS._AC_UY327_FMwebp_QL65_.jpg"
+            rating={5}
+          />
+          <Product 
+            title={"Microsoft Xbox X/S Wireless Controller Robot White"}
+            price={5199}
+            image="https://m.media-amazon.com/images/I/61MdJDbKYjL._AC_UY327_FMwebp_QL65_.jpg"
+            rating={5}
+          />
+          <Product 
+            title={"Logitech G29 Driving Force Racing Wheel and Floor Pedals"}
+            price={27995}
+            image={"https://m.media-amazon.com/images/I/61IYYoZ66VL._AC_UY327_FMwebp_QL65_.jpg"}
+            rating={5}
+          />
+        </div>
+
+        <div className="home__row">
+          <Product 
+            title={"LG Ultragear - 32Gn650 Qhd 32 Inch(80 Cm) 2560 X 1440 Pixels, 165 Hz 1Ms, Nvidia G-Sync Compatible, Freesync Premium, HDR 10, Hdmi, Tilt, Height, Pivot Stand, Va Panel Gaming LCD Monitor, Black"}
+            price={24399}
+            image="https://m.media-amazon.com/images/I/71PbKy5iBFL._AC_UY327_FMwebp_QL65_.jpg"
+            rating={5}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBtocoI2XYQ0Xh74aysvgooUOl6CnWM7qI",
+    authDomain: "kickflik-afc43.firebaseapp.com",
+    projectId: "kickflik-afc43",
+    storageBucket: "kickflik-afc43.appspot.com",
+    messagingSenderId: "43413182328",
+    appId: "1:43413182328:web:0c52217832ffb2f0029a8d",
+    measurementId: "G-M6DPTC23QL"
+  };
